b0944867df4b246353918450e016f28d448b68fa
fulgurance <zohran.londais@gmail.com>
2023-04-04 22:07:08 +0100

    Added Xorg-Applications dependencies

---
+++ Xrandr/1.5.1/1.5.1.cr
@@ -0,0 +1,31 @@
+class Target < ISM::Software
+    
+    def configure
+        super
+
+        configureSource([   "--prefix=/usr",
+                            "--sysconfdir=/etc",
+                            "--localstatedir=/var",
+                            "--disable-static"],
+                            buildDirectoryPath)
+    end
+    
+    def build
+        super
+
+        makeSource([Ism.settings.makeOptions],buildDirectoryPath)
+    end
+    
+    def prepareInstallation
+        super
+
+        makeSource([Ism.settings.makeOptions,"DESTDIR=#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}","install"],buildDirectoryPath)
+    end
+
+    def clean
+        super
+
+        deleteFile("#{Ism.settings.rootPath}usr/bin/xkeystone")
+    end
+
+end
