388cff599c6c9fd30fd620e8082d4bfe2a5e671d
fulgurance <zohran.londais@gmail.com>
2023-04-10 00:04:57 +0100

    Added new installer

---
+++ Bdftopcf/1.1.0/1.1.0.cr
@@ -0,0 +1,31 @@
+class Target < ISM::Software
+    
+    def configure
+        super
+
+        configureSource([   "--prefix=/usr",
+                            "--sysconfdir=/etc",
+                            "--localstatedir=/var",
+                            "--disable-static"],
+                            buildDirectoryPath)
+    end
+    
+    def build
+        super
+
+        makeSource(path: buildDirectoryPath)
+    end
+    
+    def prepareInstallation
+        super
+
+        makeSource(["DESTDIR=#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}","install"],buildDirectoryPath)
+    end
+
+    def install
+        super
+
+        runLdconfigCommand
+    end
+
+end
